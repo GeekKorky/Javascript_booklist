f988bb38d2700b559affa4412cdc2dfec88b68cc
GeekKorky <kaori03252014@gmail.com>
2019-01-03 18:02:28 +0800

    Add function to add books and clear fields

--- app.js
+++ app.js
@@ -43,6 +43,12 @@ class UI {
     `;
     list.appendChild(row);
   }
+
+  static clearFields() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#isbn").value = "";
+  }
 }
 
 //Store Class: Handles Storage
@@ -52,5 +58,24 @@ class UI {
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 //Event: Add a Book
+
+document.querySelector("#book-form").addEventListener("submit", e => {
+  e.preventDefault();
+
+  //Get form values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
+
+  //Instatiate Book
+  const book = new Book(title, author, isbn);
+
+  // Add book to UI
+
+  UI.addBookToList(book);
+
+  //Clear fields
+  UI.clearFields();
+});
 
 // Event: Remove a Book
