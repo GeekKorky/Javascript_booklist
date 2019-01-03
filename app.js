aaf5fd5fdc14dc3ea226e84bf76dcec12a0a261f
GeekKorky <kaori03252014@gmail.com>
2019-01-03 18:08:06 +0800

    add delete function

--- app.js
+++ app.js
@@ -44,6 +44,12 @@ class UI {
     list.appendChild(row);
   }
 
+  static deleteBook(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
   static clearFields() {
     document.querySelector("#title").value = "";
     document.querySelector("#author").value = "";
@@ -79,3 +85,7 @@ document.querySelector("#book-form").addEventListener("submit", e => {
 });
 
 // Event: Remove a Book
+
+document.querySelector("#book-list").addEventListener("click", e => {
+  UI.deleteBook(e.target);
+});
